54e7bdaaaf3a1ccecfa8af8f3def412aa151a41e
DeprecatedLuxas <40726067+DeprecatedLuxas@users.noreply.github.com>
2021-04-06 10:15:40 +0200

    feat: added workflows

---
+++ README.md
@@ -0,0 +1,11 @@
+# aws-s3-tree-structure
+
+
+## How to
+```bash
+# cd into the directory
+$ cd fluenticons-extractor
+
+# run the script
+$ python extractor.py --token [figma_api_token] --key [figma_key_file]
+```
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,47 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
+import * as fs from "fs";
 import * as AWS from "aws-sdk";
 import * as inquirer from "inquirer";
 
 let treeStructureObject: any = {};
 
 function buildTreeStructure(array: any) {
-    let map: any = treeStructureObject
+    let map: any = treeStructureObject;
     for (const item of array) {
         map[item] = map[item] || {};
         map = map[item];
     }
 }
 
-function listAllObjects(s3Client: AWS.S3, objects: any, bucket: string, callback: any, token?: string) {
+function listAllObjects(
+    s3Client: AWS.S3,
+    objects: any,
+    bucket: string,
+    callback: any,
+    token?: string
+) {
     let opts: AWS.S3.ListObjectsV2Request = {
-        Bucket: bucket
+        Bucket: bucket,
     };
     if (token) opts.ContinuationToken = token;
 
-
-
     s3Client.listObjectsV2(opts, function (err: any, data: any) {
         if (err) {
-            callback(err, null)
+            callback(err, null);
         }
         objects = objects.concat(data.Contents);
         if (data.IsTruncated) {
-            listAllObjects(s3Client, objects, bucket, callback, data.NextContinuationToken)
+            listAllObjects(
+                s3Client,
+                objects,
+                bucket,
+                callback,
+                data.NextContinuationToken
+            );
         } else {
             callback(null, objects);
         }
-    })   
+    });
 }
 
 (async () => {
@@ -54,39 +64,54 @@ function listAllObjects(s3Client: AWS.S3, objects: any, bucket: string, callback
             secretAccessKey: secretAccessKey,
         }),
     });
-    
-    const bucketList: AWS.S3.Buckets = await new Promise((success, failure) => {
-        s3.listBuckets(function (err: any, data: any) {
-            if (err) {
-                failure(err);
-            } else {
-                success(data.Buckets)
-            }
-        })
-    })
-    const { bucket } = await inquirer.prompt([
-        {
-            type: "list",
-            name: "bucket",
-            message: "Pick the correct bucket:",
-            choices: bucketList.map((bucket: AWS.S3.Bucket) => bucket.Name)
-        }
-    ]);
 
-    let objects: AWS.S3.ObjectList = []
-    let allObjects: AWS.S3.ObjectList  = await new Promise((success, failure) => {
-        listAllObjects(s3, objects, bucket, (err: any, data: any) => {
-            if (err) {
-                failure(err)
-            } else {
-                success(data)
+    try {
+        const bucketList: AWS.S3.Buckets = await new Promise(
+            (success, failure) => {
+                s3.listBuckets(function (err: any, data: any) {
+                    if (err) {
+                        failure(err);
+                    } else {
+                        success(data.Buckets);
+                    }
+                });
             }
-        })
-    })
-    allObjects.map((object: AWS.S3.Object) => object.Key).map((item: any) => item.split("/")).forEach((itemParts) => {
-        buildTreeStructure(itemParts)
-    })
-    fs.writeFileSync("./awstreestructure.json", JSON.stringify(treeStructureObject, null, 4));
-    console.log("Generated a Tree Structure file based on your s3 files.")
+        );
+        const { bucket } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "bucket",
+                message: "Pick the correct bucket:",
+                choices: bucketList.map((bucket: AWS.S3.Bucket) => bucket.Name),
+            },
+        ]);
+
+
+        let objects: AWS.S3.ObjectList = [];
+        let allObjects: AWS.S3.ObjectList = await new Promise(
+            (success, failure) => {
+                listAllObjects(s3, objects, bucket, (err: any, data: any) => {
+                    if (err) {
+                        failure(err);
+                    } else {
+                        success(data);
+                    }
+                });
+            }
+        );
+        allObjects
+            .map((object: AWS.S3.Object) => object.Key)
+            .map((item: any) => item.split("/"))
+            .forEach((itemParts) => {
+                buildTreeStructure(itemParts);
+            });
+        fs.writeFileSync(
+            "./awstreestructure.json",
+            JSON.stringify(treeStructureObject, null, 4)
+        );
+        console.log("Generated a Tree Structure file based on your s3 files.");
+    } catch (err) {
+        console.log(`Code returned with error code ${err.code}`);
+    }
+
 })();
-
